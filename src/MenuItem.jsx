473605f7a7b7e7227893f24854447f1eb98ff5f0
yiminghe <yiminghe@gmail.com>
2016-03-01 15:55:30 +0800

    support attribute for menu item

--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -119,7 +119,7 @@ const MenuItem = React.createClass({
     classes[this.getPrefixCls()] = true;
     classes[props.className] = !!props.className;
     const attrs = {
-      ...props.attributes,
+      ...props.attribute,
       title: props.title,
       className: classnames(classes),
       role: 'menuitem',
