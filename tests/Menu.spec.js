f9bda890d9b84532207b2e8c63c975c66baece7e
Wei Zhu <yesmeck@gmail.com>
2016-12-15 18:00:03 +0800

    Add more tests

--- tests/Menu.spec.js
+++ tests/Menu.spec.js
@@ -1,34 +1,165 @@
 /* eslint-disable no-undef */
 import React from 'react';
-import { mount } from 'enzyme';
+import { render, mount } from 'enzyme';
+import { renderToJson } from 'enzyme-to-json';
 import KeyCode from 'rc-util/lib/KeyCode';
-import Menu, { MenuItem, SubMenu, Divider } from '../src';
+import Menu, { MenuItem, MenuItemGroup, SubMenu, Divider } from '../src';
 
 describe('Menu', () => {
-  it('Should set the correct item active', () => {
+  describe('render', () => {
+    function createMenu(props) {
+      return (
+        <Menu
+          className="myMenu"
+          openAnimation="fade"
+          {...props}
+        >
+          <MenuItemGroup title="g1">
+            <MenuItem key="1">1</MenuItem>
+            <Divider/>
+            <MenuItem key="2">2</MenuItem>
+          </MenuItemGroup>
+          <MenuItem key="3">3</MenuItem>
+          <MenuItemGroup title="g2">
+            <MenuItem key="4">4</MenuItem>
+            <MenuItem key="5" disabled>5</MenuItem>
+          </MenuItemGroup>
+          <SubMenu title="submenu">
+            <MenuItem key="6">6</MenuItem>
+          </SubMenu>
+        </Menu>
+      );
+    }
+
+    ['vertical', 'horizontal', 'inline'].forEach((mode) => {
+      it(`renders ${mode} menu correctly`, () => {
+        const wrapper = render(createMenu({ [mode]: true }));
+        expect(renderToJson(wrapper)).toMatchSnapshot();
+      });
+    });
+  });
+
+
+  it('set activeKey', () => {
     const wrapper = mount(
-      <Menu activeKey="item1">
-        <MenuItem key="item1">Pill 1 content</MenuItem>
-        <Divider/>
-        <MenuItem key="item2">Pill 2 content</MenuItem>
+      <Menu activeKey="1">
+        <MenuItem key="1">1</MenuItem>
+        <MenuItem key="2">2</MenuItem>
       </Menu>
     );
     expect(wrapper.find('MenuItem').first().props().active).toBe(true);
     expect(wrapper.find('MenuItem').last().props().active).toBe(false);
   });
 
-  it('Should call on select when item is selected', () => {
-    const handleSelect = jest.fn();
+  it('active first item', () => {
     const wrapper = mount(
-      <Menu activeKey="item1" onSelect={handleSelect}>
-        <MenuItem key="item1" href="http://www.baidu.com">Tab 1 content</MenuItem>
-        <MenuItem key="item2">
-          Tab 2 content
-        </MenuItem>
+      <Menu defaultActiveFirst>
+        <MenuItem key="1">1</MenuItem>
+        <MenuItem key="2">2</MenuItem>
       </Menu>
     );
+    expect(wrapper.find('MenuItem').first().props().active).toBe(true);
+  });
+
+  it('select multiple items', () => {
+    const wrapper = mount(
+      <Menu multiple>
+        <MenuItem key="1">1</MenuItem>
+        <MenuItem key="2">2</MenuItem>
+      </Menu>
+    );
+    wrapper.find('MenuItem').first().simulate('click');
     wrapper.find('MenuItem').last().simulate('click');
-    expect(handleSelect.mock.calls[0][0].key).toBe('item2');
+
+    expect(wrapper.find('.rc-menu-item-selected').length).toBe(2);
+  });
+
+  it('can be controlled by selectedKeys', () => {
+    const wrapper = mount(
+      <Menu selectedKeys={['1']}>
+        <MenuItem key="1">1</MenuItem>
+        <MenuItem key="2">2</MenuItem>
+      </Menu>
+    );
+    expect(wrapper.find('li').first().props().className).toContain('-selected');
+    wrapper.setProps({ selectedKeys: ['2'] });
+    expect(wrapper.find('li').last().props().className).toContain('-selected');
+  });
+
+  it('select default item', () => {
+    const wrapper = mount(
+      <Menu defaultSelectedKeys={['1']}>
+        <MenuItem key="1">1</MenuItem>
+        <MenuItem key="2">2</MenuItem>
+      </Menu>
+    );
+    expect(wrapper.find('li').first().props().className).toContain('-selected');
+  });
+
+  it('can be controlled by openKeys', () => {
+    const wrapper = mount(
+      <Menu openKeys={['g1']}>
+        <MenuItemGroup key="g1">
+          <MenuItem key="1">1</MenuItem>
+        </MenuItemGroup>
+        <MenuItemGroup key="g2">
+          <MenuItem key="2">2</MenuItem>
+        </MenuItemGroup>
+      </Menu>
+    );
+    expect(wrapper.find('ul').first().props().className).not.toContain('-hidden');
+    wrapper.setProps({ openKeys: ['g2'] });
+    expect(wrapper.find('ul').last().props().className).not.toContain('-hidden');
+  });
+
+  it('open default submenu', () => {
+    const wrapper = mount(
+      <Menu defaultOpenKeys={['g1']}>
+        <MenuItemGroup key="g1">
+          <MenuItem key="1">1</MenuItem>
+        </MenuItemGroup>
+        <MenuItemGroup key="g2">
+          <MenuItem key="2">2</MenuItem>
+        </MenuItemGroup>
+      </Menu>
+    );
+    expect(wrapper.find('ul').first().props().className).not.toContain('-hidden');
+  });
+
+  it('fires select event', () => {
+    const handleSelect = jest.fn();
+    const wrapper = mount(
+      <Menu onSelect={handleSelect}>
+        <MenuItem key="1">1</MenuItem>
+        <MenuItem key="2">2</MenuItem>
+      </Menu>
+    );
+    wrapper.find('MenuItem').first().simulate('click');
+    expect(handleSelect.mock.calls[0][0].key).toBe('1');
+  });
+
+  it('fires click event', () => {
+    const handleClick = jest.fn();
+    const wrapper = mount(
+      <Menu onClick={handleClick}>
+        <MenuItem key="1">1</MenuItem>
+        <MenuItem key="2">2</MenuItem>
+      </Menu>
+    );
+    wrapper.find('MenuItem').first().simulate('click');
+    expect(handleClick.mock.calls[0][0].key).toBe('1');
+  });
+
+  it('fires deselect event', () => {
+    const handleDeselect = jest.fn();
+    const wrapper = mount(
+      <Menu multiple onDeselect={handleDeselect}>
+        <MenuItem key="1">1</MenuItem>
+        <MenuItem key="2">2</MenuItem>
+      </Menu>
+    );
+    wrapper.find('MenuItem').first().simulate('click').simulate('click');
+    expect(handleDeselect.mock.calls[0][0].key).toBe('1');
   });
 
   it('active by mouse enter', () => {
@@ -46,18 +177,13 @@ describe('Menu', () => {
 
   it('active by key down', () => {
     const wrapper = mount(
-      <Menu activeKey="item1">
-        <MenuItem key="item1">Pill 1 content</MenuItem>
-        <MenuItem disabled/>
-        <MenuItem key="item2">Pill 2 content</MenuItem>
-        <SubMenu key="item3" className="dropdown-submenu" title="right">
-          <MenuItem key="231">inner inner</MenuItem>
-          <MenuItem key="242">inner inner2</MenuItem>
-        </SubMenu>
+      <Menu activeKey="1">
+        <MenuItem key="1">1</MenuItem>
+        <MenuItem key="2">2</MenuItem>
       </Menu>
     );
 
     wrapper.simulate('keyDown', { keyCode: KeyCode.DOWN });
-    expect(wrapper.find('MenuItem').at(2).props().active).toBe(true);
+    expect(wrapper.find('MenuItem').at(1).props().active).toBe(true);
   });
 });
--- tests/MenuItem.spec.js
+++ tests/MenuItem.spec.js
@@ -1,29 +1,68 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import { mount } from 'enzyme';
+import KeyCode from 'rc-util/lib/KeyCode';
 import Menu, { MenuItem } from '../src';
 
 describe('MenuItem', () => {
-  it('Should add disabled class', () => {
-    const wrapper = mount(
-      <Menu>
-        <MenuItem disabled>Pill 2 content</MenuItem>
-      </Menu>
-    );
-    expect(wrapper.find('.rc-menu-item-disabled').length).toBe(1);
+  describe('disabled', () => {
+    it('can not be active by key down', () => {
+      const wrapper = mount(
+        <Menu activeKey="1">
+          <MenuItem key="1">1</MenuItem>
+          <MenuItem disabled/>
+          <MenuItem key="2">2</MenuItem>
+        </Menu>
+      );
+
+      wrapper.simulate('keyDown', { keyCode: KeyCode.DOWN });
+      expect(wrapper.find('MenuItem').at(1).props().active).toBe(false);
+    });
+
+    it('not fires select event when selected', () => {
+      const handleSelect = jest.fn();
+      const wrapper = mount(
+        <Menu>
+          <MenuItem disabled onSelect={handleSelect}>
+            <span className="xx">Item content</span>
+          </MenuItem>
+        </Menu>
+      );
+
+      wrapper.find('.xx').simulate('click');
+      expect(handleSelect).not.toBeCalled();
+    });
   });
 
-  it('Should not call `onSelect` when item disabled and is selected', () => {
-    const handleSelect = jest.fn();
-    const wrapper = mount(
-      <Menu>
-        <MenuItem disabled onSelect={handleSelect}>
-          <span className="xx">Item content</span>
-        </MenuItem>
-      </Menu>
-    );
+  describe('unmount', () => {
+    const App = ({ show }) => {
+      return (
+        <Menu>
+          {show && (
+            <MenuItem key="1">1</MenuItem>
+          )}
+        </Menu>
+      );
+    };
 
-    wrapper.find('.xx').simulate('click');
-    expect(handleSelect).not.toBeCalled();
+    it('removes self from selectedKeys', () => {
+      const wrapper = mount(<App show />);
+      wrapper.find('MenuItem').simulate('click');
+      expect(wrapper.find('Menu').node.state.selectedKeys).toEqual(['1']);
+      wrapper.setProps({ show: false });
+      expect(wrapper.find('Menu').node.state.selectedKeys).toEqual([]);
+    });
+
+    it('clears mouse leave timer', () => {
+      const wrapper = mount(<App show />);
+      const menu = wrapper.find('Menu');
+      wrapper.find('MenuItem').simulate('mouseEnter');
+      wrapper.find('MenuItem').simulate('mouseLeave');
+      expect(menu.node.menuItemMouseLeaveFn).toBeTruthy();
+      expect(menu.node.menuItemMouseLeaveTimer).toBeTruthy();
+      wrapper.setProps({ show: false });
+      expect(menu.node.menuItemMouseLeaveFn).toBe(null);
+      expect(menu.node.menuItemMouseLeaveTimer).toBe(null);
+    });
   });
 });
