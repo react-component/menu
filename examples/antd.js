4efb5c8b4a22c4f61298a91ba49d3363c9d25795
HeskeyBaozi <hezhiyu233@foxmail.com>
2018-07-30 17:05:29 +0800

    update demo

--- examples/antd.js
+++ examples/antd.js
@@ -4,16 +4,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Menu, { SubMenu, Item as MenuItem, Divider } from 'rc-menu';
 import 'rc-menu/assets/index.less';
-import 'rc-menu/assets/custom-arrow-icon.less';
 import animate from 'css-animation';
 
-const getSvgIcon = () => {
+const getSvgIcon = (style = {}) => {
   const path = 'M869 487.8L491.2 159.9c-2.9-2.5-6.6-3.9-10.5-3.9h' +
     '-88.5c-7.4 0-10.8 9.2-5.2 14l350.2 304H152c-4.4 0-8 3.6-8 8v' +
     '60c0 4.4 3.6 8 8 8h585.1L386.9 854c-5.6 4.9-2.2 14 5.2 14h91' +
     '.5c1.9 0 3.8-0.7 5.2-2L869 536.2c14.7-12.8 14.7-35.6 0-48.4z';
   return (
-    <i className={`rc-menu-submenu-arrow-icon`}>
+    <i style={style}>
       <svg
         viewBox="0 0 1024 1024"
         width="1em"
@@ -21,7 +20,7 @@ const getSvgIcon = () => {
         fill="currentColor"
         style={{ verticalAlign: '-.125em' }}
       >
-        <path d={path} p-id="5827"></path>
+        <path d={path} />
       </svg>
     </i>
   );
@@ -87,7 +86,10 @@ class Demo extends React.Component {
   }
 
   renderArrowIcon = () => {
-    return this.state.useIcon ? getSvgIcon() : undefined;
+    return this.state.useIcon ? getSvgIcon({
+      position: 'absolute',
+      right: '16px',
+    }) : undefined;
   }
 
   renderNestSubMenu = (props = {}) => {
