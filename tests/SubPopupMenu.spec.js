027f2e53af91b29f4a28fc7545adfadade810407
zombiej <smith3816@gmail.com>
2018-11-27 23:15:31 +0800

    update activeKey when changed

--- tests/SubPopupMenu.spec.js
+++ tests/SubPopupMenu.spec.js
@@ -1,4 +1,7 @@
+import React from 'react';
+import { mount } from 'enzyme';
 import { saveRef } from '../src/SubPopupMenu';
+import Menu, { MenuItem } from '../src';
 
 describe('SubPopupMenu saveRef', () => {
   let subPopupMenu;
@@ -20,5 +23,25 @@ describe('SubPopupMenu saveRef', () => {
     saveRef.call(subPopupMenu, c);
 
     expect(subPopupMenu.instanceArray).toEqual([c]);
+  });
+
+  it('activeKey change should reset store activeKey', () => {
+    const wrapper = mount(
+      <Menu activeKey="1">
+        <MenuItem key="1">1</MenuItem>
+        <MenuItem key="2">2</MenuItem>
+      </Menu>
+    );
+
+    function getItemActive(index) {
+      return wrapper.find('MenuItem').at(index).instance().props.active;
+    }
+    expect(getItemActive(0)).toBe(true);
+    expect(getItemActive(1)).toBe(false);
+
+    wrapper.setProps({ activeKey: '2' });
+
+    expect(getItemActive(0)).toBe(false);
+    expect(getItemActive(1)).toBe(true);
   });
 });
