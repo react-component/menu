5ec1b932ad299f8566ac4978d82f55c0fcd3c8b3
zombiej <smith3816@gmail.com>
2019-10-13 17:39:56 +0800

    update demo

--- examples/antd.js
+++ examples/antd.js
@@ -225,7 +225,7 @@ function Demo() {
         <div style={{ margin: 20, width: 200 }}>{verticalMenu}</div>
         <h3>inline</h3>
 
-        {/* <div style={{ margin: 20, width: 400 }}>{inlineMenu}</div> */}
+        <div style={{ margin: 20, width: 400 }}>{inlineMenu}</div>
       </div>
     </div>
   );
