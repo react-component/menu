8135a2989801a61e2d7a6a2e251c9525c282e053
zombiej <smith3816@gmail.com>
2021-05-02 10:51:15 +0800

    chore: clean up

--- src/hooks/usePathData.ts
+++
@@ -1,7 +0,0 @@
-import { parseKeys } from '../utils/nodeUtil';
-import * as React from 'react';
-
-export default function usePathData(children?: React.ReactNode) {
-  const pathData = React.useMemo(() => parseKeys(children), [children]);
-  console.log('>>>>>>', pathData);
-}
