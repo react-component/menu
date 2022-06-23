9e98ad941cfdcebed95f345202077a5d81afc225
zombiej <smith3816@gmail.com>
2022-06-23 20:28:22 +0800

    test: more and more test

--- tests/Collapsed.spec.js
+++ tests/Collapsed.spec.js
@@ -161,119 +161,128 @@ describe('Menu.Collapsed', () => {
       );
     });
 
-    // it('inlineCollapsed MenuItem Tooltip can be removed', () => {
-    //   const wrapper = mount(
-    //     <Menu
-    //       defaultOpenKeys={['not-existed']}
-    //       mode="inline"
-    //       inlineCollapsed
-    //       getPopupContainer={node => node.parentNode}
-    //     >
-    //       <MenuItem key="menu1">item</MenuItem>
-    //       <MenuItem key="menu2" title="title">
-    //         item
-    //       </MenuItem>
-    //       <MenuItem key="menu3" title={undefined}>
-    //         item
-    //       </MenuItem>
-    //       <MenuItem key="menu4" title={null}>
-    //         item
-    //       </MenuItem>
-    //       <MenuItem key="menu5" title="">
-    //         item
-    //       </MenuItem>
-    //       <MenuItem key="menu6" title={false}>
-    //         item
-    //       </MenuItem>
-    //     </Menu>,
-    //   );
-    //   expect(wrapper.find(MenuItem).at(0).getDOMNode().title).toBe('');
-    //   expect(wrapper.find(MenuItem).at(1).getDOMNode().title).toBe('title');
-    //   expect(wrapper.find(MenuItem).at(2).getDOMNode().title).toBe('');
-    //   expect(wrapper.find(MenuItem).at(3).getDOMNode().title).toBe('');
-    //   expect(wrapper.find(MenuItem).at(4).getDOMNode().title).toBe('');
-    //   expect(wrapper.find(MenuItem).at(4).getDOMNode().title).toBe('');
-    // });
+    it('inlineCollapsed MenuItem Tooltip can be removed', () => {
+      const { container } = render(
+        <Menu
+          defaultOpenKeys={['not-existed']}
+          mode="inline"
+          inlineCollapsed
+          getPopupContainer={node => node.parentNode}
+        >
+          <MenuItem key="menu1">item</MenuItem>
+          <MenuItem key="menu2" title="title">
+            item
+          </MenuItem>
+          <MenuItem key="menu3" title={undefined}>
+            item
+          </MenuItem>
+          <MenuItem key="menu4" title={null}>
+            item
+          </MenuItem>
+          <MenuItem key="menu5" title="">
+            item
+          </MenuItem>
+          <MenuItem key="menu6" title={false}>
+            item
+          </MenuItem>
+        </Menu>,
+      );
 
-    // // https://github.com/ant-design/ant-design/issues/18825
-    // // https://github.com/ant-design/ant-design/issues/8587
-    // it('should keep selectedKeys in state when collapsed to 0px', () => {
-    //   const wrapper = mount(
-    //     <Menu
-    //       mode="inline"
-    //       inlineCollapsed={false}
-    //       defaultSelectedKeys={['1']}
-    //       openKeys={['3']}
-    //     >
-    //       <MenuItem key="1">Option 1</MenuItem>
-    //       <MenuItem key="2">Option 2</MenuItem>
-    //       <SubMenu key="3" title="Option 3">
-    //         <MenuItem key="4">Option 4</MenuItem>
-    //       </SubMenu>
-    //     </Menu>,
-    //   );
+      expect(
+        Array.from(container.querySelectorAll('.rc-menu-item')).map(
+          node => node.title,
+        ),
+      ).toEqual(['', 'title', '', '', '', '']);
+    });
 
-    //   // Default
-    //   expect(
-    //     wrapper.find('li.rc-menu-item-selected').getDOMNode().textContent,
-    //   ).toBe('Option 1');
+    // https://github.com/ant-design/ant-design/issues/18825
+    // https://github.com/ant-design/ant-design/issues/8587
+    it('should keep selectedKeys in state when collapsed to 0px', () => {
+      const genMenu = props => (
+        <Menu
+          mode="inline"
+          inlineCollapsed={false}
+          defaultSelectedKeys={['1']}
+          openKeys={['3']}
+          {...props}
+        >
+          <MenuItem key="1">Option 1</MenuItem>
+          <MenuItem key="2">Option 2</MenuItem>
+          <SubMenu key="3" title="Option 3">
+            <MenuItem key="4">Option 4</MenuItem>
+          </SubMenu>
+        </Menu>
+      );
 
-    //   // Click to change select
-    //   wrapper.find('li.rc-menu-item').at(1).simulate('click');
-    //   expect(
-    //     wrapper.find('li.rc-menu-item-selected').getDOMNode().textContent,
-    //   ).toBe('Option 2');
+      const { container, rerender } = render(genMenu());
 
-    //   // Collapse it
-    //   wrapper.setProps({ inlineCollapsed: true });
-    //   act(() => {
-    //     jest.runAllTimers();
-    //     wrapper.update();
-    //   });
+      // Default
+      expect(
+        container.querySelector('.rc-menu-item-selected').textContent,
+      ).toBe('Option 1');
 
-    //   // Open since controlled
-    //   expect(wrapper.find('Trigger').props().popupVisible).toBeTruthy();
+      // Click to change select
+      fireEvent.click(container.querySelectorAll('.rc-menu-item')[1]);
+      expect(
+        container.querySelector('.rc-menu-item-selected').textContent,
+      ).toBe('Option 2');
 
-    //   // Expand it
-    //   wrapper.setProps({ inlineCollapsed: false });
-    //   expect(
-    //     wrapper.find('li.rc-menu-item-selected').getDOMNode().textContent,
-    //   ).toBe('Option 2');
-    // });
+      // Collapse it
+      rerender(genMenu({ inlineCollapsed: true }));
+      act(() => {
+        jest.runAllTimers();
+      });
 
-    // it('should hideMenu in initial state when collapsed', () => {
-    //   const wrapper = mount(
-    //     <Menu
-    //       mode="inline"
-    //       inlineCollapsed
-    //       defaultSelectedKeys={['1']}
-    //       openKeys={['3']}
-    //     >
-    //       <MenuItem key="1">Option 1</MenuItem>
-    //       <MenuItem key="2">Option 2</MenuItem>
-    //       <SubMenu key="3" title="Option 3">
-    //         <MenuItem key="4">Option 4</MenuItem>
-    //       </SubMenu>
-    //     </Menu>,
-    //   );
+      // Open since controlled
+      expect(container.querySelector('.rc-menu-submenu-popup')).toBeTruthy();
 
-    //   expect(wrapper.find('Trigger').props().popupVisible).toBeFalsy();
+      // Expand it
+      rerender(genMenu({ inlineCollapsed: false }));
+      expect(
+        container.querySelector('.rc-menu-item-selected').textContent,
+      ).toBe('Option 2');
+    });
 
-    //   wrapper.setProps({ inlineCollapsed: false });
-    //   act(() => {
-    //     jest.runAllTimers();
-    //     wrapper.update();
-    //   });
-    //   expect(
-    //     wrapper.find('li.rc-menu-item-selected').getDOMNode().textContent,
-    //   ).toBe('Option 1');
-    // });
+    it('should hideMenu in initial state when collapsed', () => {
+      const genMenu = props => (
+        <Menu
+          mode="inline"
+          inlineCollapsed
+          defaultSelectedKeys={['1']}
+          openKeys={['3']}
+          {...props}
+        >
+          <MenuItem key="1">Option 1</MenuItem>
+          <MenuItem key="2">Option 2</MenuItem>
+          <SubMenu key="3" title="Option 3">
+            <MenuItem key="4">Option 4</MenuItem>
+          </SubMenu>
+        </Menu>
+      );
 
-    // it('vertical also support inlineCollapsed', () => {
-    //   const wrapper = mount(<Menu mode="vertical" inlineCollapsed />);
+      const { container, rerender } = render(genMenu());
 
-    //   expect(wrapper.exists('.rc-menu-inline-collapsed')).toBeTruthy();
-    // });
+      act(() => {
+        jest.runAllTimers();
+      });
+
+      expect(container.querySelector('.rc-menu-submenu-popup')).toBeTruthy();
+
+      rerender(genMenu({ inlineCollapsed: false }));
+      act(() => {
+        jest.runAllTimers();
+      });
+
+      expect(
+        container.querySelector('.rc-menu-item-selected').textContent,
+      ).toBe('Option 1');
+    });
+
+    it('vertical also support inlineCollapsed', () => {
+      const { container } = render(<Menu mode="vertical" inlineCollapsed />);
+
+      expect(container.querySelector('.rc-menu-inline-collapsed')).toBeTruthy();
+    });
   });
 });
 /* eslint-enable */
