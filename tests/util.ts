2ce37d0f2bf72df1534b54ef728faa65854d88b1
zombiej <smith3816@gmail.com>
2022-06-24 11:59:48 +0800

    test: keyboard test case

--- tests/util.ts
+++ tests/util.ts
@@ -1,15 +1,29 @@
-import { mount as originMount } from 'enzyme';
-import type {
-  ReactWrapper as OriginReactWrapper,
-  MountRendererProps,
-} from 'enzyme';
+// import { mount as originMount } from 'enzyme';
+// import type {
+//   ReactWrapper as OriginReactWrapper,
+//   MountRendererProps,
+// } from 'enzyme';
 
-export type ReactWrapper = OriginReactWrapper & {
-  flush: () => ReactWrapper;
-  isActive: (index?: number) => boolean;
-};
+// export type ReactWrapper = OriginReactWrapper & {
+//   flush: () => ReactWrapper;
+//   isActive: (index?: number) => boolean;
+// };
 
-export const mount = originMount as (
-  node: React.ReactElement,
-  options?: MountRendererProps,
-) => ReactWrapper;
+// export const mount = originMount as (
+//   node: React.ReactElement,
+//   options?: MountRendererProps,
+// ) => ReactWrapper;
+
+export function isActive(container: HTMLElement, index: number, active = true) {
+  const checker = expect(container.querySelectorAll('li.rc-menu-item')[index]);
+
+  if (active) {
+    checker.toHaveClass('rc-menu-item-active');
+  } else {
+    checker.not.toHaveClass('rc-menu-item-active');
+  }
+}
+
+export function last(elements: NodeListOf<Element>) {
+  return elements[elements.length - 1];
+}
