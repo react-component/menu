fd4cbc1ae35b3baffe10e5a0edce056f744228a7
zombiej <smith3816@gmail.com>
2022-06-26 12:36:39 +0800

    chore: cleanup

--- tests/setup.js
+++ tests/setup.js
@@ -3,26 +3,3 @@ window.requestAnimationFrame = func => {
   return id;
 };
 window.cancelAnimationFrame = id => window.clearTimeout(id);
-
-// const Enzyme = require('enzyme');
-// const Adapter = require('enzyme-adapter-react-16');
-// require('regenerator-runtime/runtime');
-
-// Enzyme.configure({ adapter: new Adapter() });
-
-// Object.assign(Enzyme.ReactWrapper.prototype, {
-//   findItem(index = 0) {
-//     return this.find('li.rc-menu-item').at(index);
-//   },
-//   isActive(index = 0) {
-//     return this.findItem(index).hasClass('rc-menu-item-active');
-//   },
-//   async flush() {
-//     for (let i = 0; i < 3; i += 1) {
-//       // eslint-disable-next-line no-await-in-loop
-//       await Promise.resolve();
-//     }
-
-//     return this;
-//   },
-// });
--- tests/util.ts
+++ tests/util.ts
@@ -1,19 +1,3 @@
-// import { mount as originMount } from 'enzyme';
-// import type {
-//   ReactWrapper as OriginReactWrapper,
-//   MountRendererProps,
-// } from 'enzyme';
-
-// export type ReactWrapper = OriginReactWrapper & {
-//   flush: () => ReactWrapper;
-//   isActive: (index?: number) => boolean;
-// };
-
-// export const mount = originMount as (
-//   node: React.ReactElement,
-//   options?: MountRendererProps,
-// ) => ReactWrapper;
-
 export function isActive(container: HTMLElement, index: number, active = true) {
   const checker = expect(container.querySelectorAll('li.rc-menu-item')[index]);
 
