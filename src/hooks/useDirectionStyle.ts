2c4e96a79799b4e3a2222d46a74792dd61509749
MadCcc <1075746765@qq.com>
2022-08-04 11:34:06 +0800

    Revert "feat: Ignore top level indent (#498)"
    This reverts commit fa7e3509f08b3d6fa9f54c5ae94947855d278ea3.

--- src/hooks/useDirectionStyle.ts
+++ src/hooks/useDirectionStyle.ts
@@ -8,7 +8,7 @@ export default function useDirectionStyle(level: number): React.CSSProperties {
     return null;
   }
 
-  const len = Math.max(0, level - 1);
+  const len = level;
   return rtl
     ? { paddingRight: len * inlineIndent }
     : { paddingLeft: len * inlineIndent };
