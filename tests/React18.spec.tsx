0cf3d90cca69d46266c1324edc0c0f793e844f0d
huangkairan <56213366+huangkairan@users.noreply.github.com>
2022-10-27 18:31:57 +0800

    fix: SubMenu in React18 sync problem (#537)
    Co-authored-by: huangkairan <wb-hkr877030@alibaba-inc.com>

--- tests/React18.spec.tsx
+++ tests/React18.spec.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 import React from 'react';
-import { act, render } from '@testing-library/react';
+import { act, fireEvent, render } from '@testing-library/react';
+import { sleep } from './util';
 import Menu, { MenuItem, SubMenu } from '../src';
 import type { MenuProps } from '../src';
 
@@ -54,6 +55,40 @@ describe('React18', () => {
         .querySelector('.rc-menu-submenu-open')
         .querySelector('.rc-menu-submenu-title').textContent,
     ).toEqual('submenu1');
+  });
+
+  it('prevent React 18 auto batch', async () => {
+    const handleOpenChange = jest.fn();
+    const { container } = render(
+      <Menu onOpenChange={handleOpenChange}>
+        <SubMenu title="s1">
+          <MenuItem key="1">1</MenuItem>
+        </SubMenu>
+        <SubMenu title="s2">
+          <MenuItem key="2">2</MenuItem>
+        </SubMenu>
+      </Menu>,
+    );
+
+    // Enter
+    fireEvent.mouseEnter(container.querySelector('.rc-menu-submenu-title'));
+    runAllTimer();
+    expect(container.querySelector('.rc-menu-submenu-open')).toBeTruthy();
+    // Leave
+    fireEvent.mouseLeave(container.querySelector('.rc-menu-submenu-title'));
+    act(() => {
+      jest.runAllTimers();
+    });
+    expect(container.querySelector('.rc-menu-submenu-open')).toBeFalsy();
+    await act(async () => {
+      await sleep();
+    });
+    // Enter
+    fireEvent.mouseEnter(
+      container.querySelectorAll('.rc-menu-submenu-title')[1],
+    );
+    jest.runAllTimers();
+    expect(container.querySelector('.rc-menu-submenu-open')).toBeTruthy();
   });
 });
 /* eslint-enable */
