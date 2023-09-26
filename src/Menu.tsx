9143b72870d5397e2835a2c1e2afe42417ffa7ff
huan-qiu <huan.qiu183@gmail.com>
2023-09-26 17:02:28 +1300

    fix: add prop to opt out of ssr full

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -78,6 +78,9 @@ export interface MenuProps
   activeKey?: string;
   defaultActiveFirst?: boolean;
 
+  // SSR control
+  ssr?: 'full' | 'off';
+
   // Selection
   selectable?: boolean;
   multiple?: boolean;
@@ -183,6 +186,9 @@ const Menu = React.forwardRef<MenuRef, MenuProps>((props, ref) => {
     activeKey,
     defaultActiveFirst,
 
+    // SSR
+    ssr,
+
     // Selection
     selectable = true,
     multiple = false,
@@ -239,6 +245,7 @@ const Menu = React.forwardRef<MenuRef, MenuProps>((props, ref) => {
   const uuid = useUUID(id);
 
   const isRtl = direction === 'rtl';
+  const ssrVal = ssr === 'off' ? undefined : 'full';
 
   // ========================= Warn =========================
   if (process.env.NODE_ENV !== 'production') {
@@ -581,7 +588,7 @@ const Menu = React.forwardRef<MenuRef, MenuProps>((props, ref) => {
           ? Overflow.INVALIDATE
           : Overflow.RESPONSIVE
       }
-      ssr="full"
+      ssr={ssrVal}
       data-menu-list
       onVisibleChange={newLastIndex => {
         setLastVisibleIndex(newLastIndex);
